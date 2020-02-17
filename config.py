41ffa7a595e949b42949c0dbf4659eb64e36a58d
Nacool97 <nakulsatishkulkarni@gmail.com>
2020-02-18 00:54:56 +0530

    commit 1

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '1014299293:AAFQIQeYKlvOgR3eHQc6TMNLrtq8RPafmqY'
+NGROK_URL = 'https://a088abe1.ngrok.io'
+BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
+LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
+TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
+TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}'
